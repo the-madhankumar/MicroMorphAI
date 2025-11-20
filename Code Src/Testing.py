515b34a7c9ec4bf2f2b2114e87d32613b1feab7e
Madhan Kumar <madhankumarbusiness@gmail.com>
2025-11-20 11:47:36 +0530

    contrast enhancement

--- Code Src/Testing.py
+++ Code Src/Testing.py
@@ -1,4 +1,9 @@
+import cv2
 from microvision.imagePreprocessingPipline.grayScaleConverstion import PCAGrayscaleConverter
+from microvision.imagePreprocessingPipline.contrastEnhancement import CLAHEEnhancer
 
 converter = PCAGrayscaleConverter("D:/projects/Project MicroMorph AI/Images/TestImages/grayscale.webp")
-converter.show()
+grayscale_value = converter.convert_to_grayscale()
+
+enhancer = CLAHEEnhancer(grayscale_mat=grayscale_value)
+enhancer.show(title="CLAHE Output", axis="off")
